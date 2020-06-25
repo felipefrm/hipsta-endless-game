c7cf3cbc89546ee5b972a2e4ef1ad25a8273b6a6
Felipe Melo <narulee@hotmail.com>
2020-06-24 22:30:14 -0300

    dia 3 - adicionando novos inimigos e pontuação

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,41 +1,44 @@
-
-class Personagem extends Animacao{
-    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
-    this.yInicial = height - this.altura;
+class Personagem extends Animacao {
+  constructor(matriz, imagem, x,variacaoY  , largura, altura, larguraSprite, alturaSprite) {
+     super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - this.variacaoY
     this.y = this.yInicial;
-    this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
+    this.velocidadeDoPulo = 0
+    this.gravidade = 6;
+    this.alturaJump = -50;
+    this.jump = 0;
+    }
     
-  }
-  pula(){
-     this.velocidadeDoPulo = -30;
+    pula(){
+    if(this.jump < 2){
+      this.velocidadeDoPulo = this.alturaJump;
+      this.jump++;
+    }
   }
   
-  aplicaGravidade(){
+  aplicaGravidade() {
     this.y += this.velocidadeDoPulo;
-    this.velocidadeDoPulo += this.gravidade; 
+    this.velocidadeDoPulo = this.velocidadeDoPulo +  this.gravidade;
     
     if(this.y > this.yInicial){
       this.y = this.yInicial;
+      this.jump = 0;
     }
-    
   }
   
-  estaColidindo(inimigo){
-    const precisao = .7;
+  estaColidindo(inimigo) {
+    const precisao = 0.7
     const colisao = collideRectRect(
-      this.x, 
-      this.y, 
-      this.largura * precisao, 
-      this.altura * precisao,
-      inimigo.x,
-      inimigo.y,
-      inimigo.largura * precisao,
-      inimigo.altura * precisao
+        this.x,
+        this.y,
+        this.largura * precisao, 
+        this.altura * precisao,
+        inimigo.x,
+        inimigo.y,
+        inimigo.largura * precisao,
+        inimigo.altura * precisao
     );
-    
-    return colisao;
-    
+      return colisao;
   }
 }
