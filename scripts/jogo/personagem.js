c0a8ce9d13aa04b26f6d7591f7591e7d5eadad35
Felipe Melo <narulee@hotmail.com>
2020-06-25 22:10:33 -0300

    aula 4 - aprimorando a frequencia de inimigos e criando a tela inicial

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,44 +1,53 @@
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x,variacaoY  , largura, altura, larguraSprite, alturaSprite) {
-     super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
-    this.variacaoY = variacaoY;
-    this.yInicial = height - this.altura - this.variacaoY
-    this.y = this.yInicial;
-    this.velocidadeDoPulo = 0
-    this.gravidade = 6;
-    this.alturaJump = -50;
-    this.jump = 0;
-    }
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
     
-    pula(){
-    if(this.jump < 2){
-      this.velocidadeDoPulo = this.alturaJump;
-      this.jump++;
+    this.variacaoY = variacaoY;
+    
+    this.yInicial = height - this.altura - this.variacaoY;
+    this.y = this.yInicial;
+    
+    this.velocidadeDoPulo = 0;
+    this.gravidade = 3;
+    this.alturaDoPulo = -30;
+    this.pulos = 0;
+  }
+  
+  pula(){
+    if (this.pulos < 2) {
+      //this.velocidadeDoPulo = this.velocidadeDoPulo - 50;
+      this.velocidadeDoPulo = this.alturaDoPulo;
+      this.pulos++;
     }
   }
   
   aplicaGravidade() {
-    this.y += this.velocidadeDoPulo;
-    this.velocidadeDoPulo = this.velocidadeDoPulo +  this.gravidade;
+    this.y = this.y + this.velocidadeDoPulo;
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
     
-    if(this.y > this.yInicial){
+    //Momento em que a personagem toca o chão.
+    if (this.y > this.yInicial) {
       this.y = this.yInicial;
-      this.jump = 0;
+      this.pulos = 0;
     }
   }
   
   estaColidindo(inimigo) {
-    const precisao = 0.7
+    //noFill();
+    //rect(this.x, this.y, this.largura, this.altura);
+    //rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
+    const precisao = 0.65;
     const colisao = collideRectRect(
-        this.x,
-        this.y,
-        this.largura * precisao, 
-        this.altura * precisao,
-        inimigo.x,
-        inimigo.y,
-        inimigo.largura * precisao,
-        inimigo.altura * precisao
+      this.x,
+      this.y,
+      this.largura * precisao,
+      this.altura * precisao,
+      inimigo.x,
+      inimigo.y,
+      inimigo.largura * precisao,
+      inimigo.altura * precisao
     );
-      return colisao;
+    return colisao;
+    //return false;
   }
 }
